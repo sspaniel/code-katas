e6287a5160dca8dad0300e13cd2751285530d31b
Sean Spaniel <sean.spaniel@gmail.com>
2025-10-06 18:38:35 -0400

    spike problem solution

---
+++ Code.Katas/Code.Katas.Arrays/Spike/Solution.cs
@@ -0,0 +1,75 @@
+﻿namespace Code.Katas.Arrays.Spike;
+
+public static class Solution
+{
+    /// <summary>
+    /// Given an array of integers, returns the longest spike that can be formed from the array.
+    /// A spike is defined as a sequence of numbers that first strictly increases and then strictly decreases.
+    /// </summary>
+    public static int[] Execute(int[] values)
+    {
+        if (values.Length < 2)
+        {
+            return [];
+        }
+
+        var sortedValues = values.OrderBy(v => v).ToArray();
+
+        var frequencies = new Dictionary<int, int>();
+        var queue = new Queue<int>();
+
+        for (int i = 0; i < sortedValues.Length; i++) 
+        {
+            var current = sortedValues[i];
+
+            if (!frequencies.ContainsKey(current))
+            {
+                // initialize frequency for the current number
+                frequencies[current] = 0;
+            }
+
+            // increment frequency for the current number
+            frequencies[current]++;
+
+            if (frequencies[current] > 2)
+            {
+                // skip numbers that appear more than twice
+                continue;
+            }
+            
+            queue.Enqueue(current);
+        }
+
+        // use two pointers to build the spike from the outside in
+        var spike = new int[queue.Count];
+        var isLeft = true;
+        var left = 0;
+        var rigth = spike.Length - 1;
+
+        while (queue.Count > 0)
+        { 
+            var current = queue.Dequeue();
+
+            if (queue.Count == 1 && current == queue.Peek())
+            {
+                // skip the last number if it's a duplicate
+                break;
+            }
+
+            if (isLeft)
+            {
+                spike[left] = current;
+                left++;
+                isLeft = false;
+            }
+            else
+            {
+                spike[rigth] = current;
+                rigth--;
+                isLeft = true;
+            }
+        }
+
+        return spike;
+    }
+}
---
+++ Code.Katas/Code.Katas.Arrays/Spike/Tests.cs
@@ -0,0 +1,45 @@
+﻿namespace Code.Katas.Arrays.Spike;
+
+public class Tests
+{
+    [Test]
+    [TestCase(new int[0])]
+    [TestCase(new int[] { 1 })]
+    public void ReturnsEmpty_WhenGivenShortArray(int[] values)
+    {
+        // arrange
+        var expected = new int[0];
+
+        // act
+        var result = Solution.Execute(values);
+
+        // assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void ReturnsSpike_WhenGivenArrayWithSpike()
+    {
+        // arrange
+        var values   = new int[] { 1, 2, 3, 4, 5, 4, 3, 2, 1 };
+        var expected = new int[] { 1, 2, 3, 4, 5, 4, 3, 2, 1 };
+
+        // act
+        var result = Solution.Execute(values);
+        // assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void ReturnsSpike_WhenGivenArrayWithSpikeAndManyDuplicateValues()
+    {
+        // arrange
+        var values = new int[] { 1, 13, 8, 4, 1, 7, 5, 8, 1, 3, 1, 9, 12 };
+        var expected = new int[] { 1, 3, 5, 8, 9, 13, 12, 8, 7, 4, 1};
+
+        // act
+        var result = Solution.Execute(values);
+        // assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
